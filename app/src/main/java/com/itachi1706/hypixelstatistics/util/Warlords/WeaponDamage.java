e3ff7544593679b60d2649b8040b4991ea11400b
itachi1706 <kennethsohyq@hotmail.com>
2015-03-09 22:06:34 +0800

    Quick patch before releasing new build to add in more damage ids that i found

--- app/src/main/java/com/itachi1706/hypixelstatistics/util/Warlords/WeaponDamage.java
+++ app/src/main/java/com/itachi1706/hypixelstatistics/util/Warlords/WeaponDamage.java
@@ -26,6 +26,7 @@ public enum WeaponDamage {
     NINETY_NINE("84 - 113", 84, 113, 99),
     ONE_HUNDERED("85 - 115", 85, 115, 100),
     ONE_HUNDRED_ONE("85 - 116 ", 85, 116, 101),
+    ONE_HUNDERED_TEN("93 - 126", 93, 126, 110),
     WIP("Work in Progress (WIP)", 0, 0, 0);
 
     private final String damageRangeString;
@@ -63,6 +64,7 @@ public enum WeaponDamage {
             case 99: return NINETY_NINE;
             case 100: return ONE_HUNDERED;
             case 101: return ONE_HUNDRED_ONE;
+            case 110: return ONE_HUNDERED_TEN;
             default: return WIP;
         }
     }
