1d3a1d68bb6b0220c47ab7f35b80aba60e04153d
itachi1706 <kennethsohyq@hotmail.com>
2015-04-11 22:29:54 +0800

    Started implementing Action Bar SearchView ability

--- app/src/main/java/com/itachi1706/hypixelstatistics/FriendListActivity.java
+++ app/src/main/java/com/itachi1706/hypixelstatistics/FriendListActivity.java
@@ -1,23 +1,66 @@
 package com.itachi1706.hypixelstatistics;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.itachi1706.hypixelstatistics.AsyncAPI.Friends.GetFriendsListUUID;
 
 
 public class FriendListActivity extends ActionBarActivity {
+
+    TextView friendsCount;
+    ListView friendListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_list);
+
+        friendsCount = (TextView) findViewById(R.id.friendCount);
+        friendListView = (ListView) findViewById(R.id.lvFriendList);
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        Log.d("FRIEND-INTENT", "Received an intent. Passing it through");
+        //handleIntent(getIntent());
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent){
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())){
+            Log.d("FRIEND-INTENT", "Search intent found. Processing it now...");
+            String searchQuery = intent.getStringExtra(SearchManager.QUERY);
+            Log.d("FRIEND-INTENT", "Search query in intent is: " + searchQuery);
+            //Do stuff with query
+            friendsCount.setText("You asked for: " + searchQuery);
+            Toast.makeText(this.getApplicationContext(), "Retrieving Friend List of " + searchQuery, Toast.LENGTH_SHORT).show();
+            new GetFriendsListUUID(this, friendListView, friendsCount).execute(searchQuery);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_friend_list, menu);
+
+        //Associate searchable config with SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
