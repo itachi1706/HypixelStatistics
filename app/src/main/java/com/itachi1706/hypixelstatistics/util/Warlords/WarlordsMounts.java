d5a234e73b02cfda845a7f432142d15744c5e318
itachi1706 <kennethsohyq@hotmail.com>
2015-05-05 20:23:07 +0800

    Added the relevant colors for Battle Beast and War Horse - Battle Beast is Blue - War Horse is Blue

--- app/src/main/java/com/itachi1706/hypixelstatistics/util/Warlords/WarlordsMounts.java
+++ app/src/main/java/com/itachi1706/hypixelstatistics/util/Warlords/WarlordsMounts.java
@@ -4,13 +4,12 @@ package com.itachi1706.hypixelstatistics.util.Warlords;
  * Created by Kenneth on 2/5/2015
  * for HypixelStatistics in package com.itachi1706.hypixelstatistics.util.Warlords
  */
-//TODO Get actual color for WarHorse and BattleBeast
 public enum WarlordsMounts {
     NOBLE_STEED("noble_steed", "§7Noble Steed§r"),
     UNDYING_MARE("undying_mare", "§aUndying Mare§r"),
     CORPSE_MARE("corpse_mare", "§aCorpse Mare§r"),
-    WAR_HORSE("war_horse", "§aWar Horse§r"),
-    BATTLE_BEAST("battle_beast", "§aBattle Beast§r"),
+    WAR_HORSE("war_horse", "§9War Horse§r"),
+    BATTLE_BEAST("battle_beast", "§9Battle Beast§r"),
     RAGING_STALLION("raging_stallion", "§9Raging Stallion§r"),
     UNKNOWN("unknown", "Unknown");
 
