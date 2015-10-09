442a9f3c3975169680b747b1c5020bc4aa8fe378
itachi1706 <kennethsohyq@gmail.com>
2015-10-09 20:43:42 +0800

    Tweaked the initials generation for the placeholder image

--- app/src/main/java/com/itachi1706/hypixelstatistics/util/GeneratePlaceholderDrawables.java
+++ app/src/main/java/com/itachi1706/hypixelstatistics/util/GeneratePlaceholderDrawables.java
@@ -3,14 +3,16 @@ package com.itachi1706.hypixelstatistics.util;
 import com.amulyakhare.textdrawable.TextDrawable;
 import com.amulyakhare.textdrawable.util.ColorGenerator;
 
-import org.w3c.dom.Text;
-
 /**
  * Created by Kenneth on 9/10/2015.
  * for HypixelStatistics in package com.itachi1706.hypixelstatistics.util
  */
 public class GeneratePlaceholderDrawables {
 
+    /**
+     * Creates and return the builder for TextDrawable
+     * @return The TextDrawable builder
+     */
     private static TextDrawable.IBuilder getBuilder(){
         return TextDrawable.builder()
                 .beginConfig().withBorder(4)
@@ -18,6 +20,11 @@ public class GeneratePlaceholderDrawables {
                 .endConfig().rect();
     }
 
+    /**
+     * Generate the TextDrawable from the Initials
+     * @param name Initials to generate drawable from
+     * @return The TextDrawable object
+     */
     private static TextDrawable generateFromMcNameInitials(String name){
         ColorGenerator generator = ColorGenerator.MATERIAL;
         int color = generator.getColor("mcPlayerName");
@@ -25,15 +32,34 @@ public class GeneratePlaceholderDrawables {
         return builder.build(name, color);
     }
 
+    /**
+     * Generates a up to 2-letter initial from a string
+     * @param name The string to generate
+     * @return up to 2-letters initials
+     */
     private static String generateInitialsFromName(String name){
-        String[] inits = name.split(" ");
-        String initial = "";
-        for (String s : inits){
-            initial += s.charAt(0);
+        String val = "";
+        boolean space = true;
+        for (int i = 0; i < name.length(); i++){
+            char c = name.charAt(i);
+            if (Character.isUpperCase(c)) val += c;
+            else if (!Character.isLetter(c) && !Character.isDigit(c)) val += c;
+            else if (Character.isDigit(c) && space) { val += c; }
+            else if (Character.isSpaceChar(c)) { space = true; continue; }
+            else if (space) val += c;
+            space = false;
         }
-        return initial;
+
+        //Only take first and last char
+        return val.length() > 1 ? val.substring(0, 1) + val.substring(val.length() - 1, val.length()) : val.substring(0,1);
     }
 
+
+    /**
+     * Generate a up to 2-letter TextDrawable from name, including the extraction of initials
+     * @param name a string to extract initials and generate the drawable from
+     * @return Generated drawable
+     */
     public static TextDrawable generateFromMcNameWithInitialsConversion(String name){
         return generateFromMcNameInitials(generateInitialsFromName(name));
     }
